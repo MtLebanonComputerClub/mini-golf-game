3f2a633a698e732a027cf5017877a6ce8548030d
alphafishprop <rayrobson1.0@gmail.com>
2015-05-15 09:14:22 -0400

    Update Interaction Interface.vb

--- Mini Golf/Interaction Interface.vb
+++ Mini Golf/Interaction Interface.vb
@@ -1,5 +1,5 @@
 ﻿Public Class Interaction_Interface
-    Public Sub friction(ball As Ball, surface as Surface)
+    Public Sub friction(ball As Ball, surface as Surface)'Still need Ball object, Surface object
         'using Java syntax, since there is no autofill on this
         Dim i as Int=ball.getHistory.length()
         Dim v1 as Int=ball.getHistory[i-1]-ball.getHistory[i-2]
