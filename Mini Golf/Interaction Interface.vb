ee174a56dc85f287565c5952d8faccdeef859b32
alphafishprop <rayrobson1.0@gmail.com>
2015-05-27 16:59:14 -0400

    Tried to add collide. Failed.
    Didn't use brain. Will fix soon.

--- Mini Golf/Interaction Interface.vb
+++ Mini Golf/Interaction Interface.vb
@@ -1,10 +1,13 @@
 ﻿Public Class Interaction_Interface
     Public Sub friction(ball As Ball, surface as Surface)'Still need Ball object, Surface object
         'using Java syntax, since there is no autofill on this
-        Dim i as Int=ball.getHistory.length()
-        Dim v1 as Int=ball.getHistory[i-1]-ball.getHistory[i-2]
-        Dim frict as Int=surface.getFriction();
-        Dim out=-frict+v1+ball.getHistory[i-1];
-        return out
+        'Dim i As int = ball.gethistory.length()
+        'dim v1 as int=ball.gethistory[i-1]-ball.gethistory[i-2]
+        'dim frict as int=surface.getfriction();
+        'dim out=-frict+v1+ball.gethistory[i-1];
+        'Return out
+    End Sub
+    Public Sub collide(ball As Ball, wall As Wall)
+        Dim 
     End Sub
 End Class
